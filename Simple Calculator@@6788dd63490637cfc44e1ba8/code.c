30c11edb61fd88db4636ad211ab092f597ae289a
ujjwal1614 <ujjwal.1614@gmail.com>
2025-02-05 10:18:03 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,6 +4,21 @@ int main() {
     int a, b;
     char c;
     scanf("%d %d %c", &a, &b, &c);
-    printf("%c", c);
+    if(c == "+"){
+        int add = a+b;
+        printf("%d", add);
+    }
+    else if(c == "-"){
+        int substract = a-b;
+        printf("%d", substract);
+    }
+    else if(c == "/"){
+        int devide = a/b;
+        printf("%d", devide);
+    }
+    else if(c == "*"){
+        int multiply = a*b;
+        printf("%d", multiply);
+    }
     return 0;
 }
