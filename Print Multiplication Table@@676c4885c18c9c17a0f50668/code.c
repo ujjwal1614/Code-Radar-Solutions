f17baca94f4e85c154ee4b812c24b33ac99c1bc0
ujjwal1614 <ujjwal.1614@gmail.com>
2025-02-11 19:03:23 +0530

    Solution to Print Multiplication Table

--- Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int n, m;
     scanf("%d", &n);
     for(int i =0; i<n; i++){
         printf("2 x %d ", i+1);
-        int m;
-        m = 2*(i+1);
+        m = n*(i+1);
         printf("= %d", m);
     }
     return 0;
