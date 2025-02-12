834e587c7837ba2e9292e221cab70de7b12f5220
ujjwal1614 <ujjwal.1614@gmail.com>
2025-02-12 11:11:44 +0530

    Solution to Grade Evaluation

---
+++ Grade Evaluation@@6788dd63490637cfc44e1bab/code.c
@@ -0,0 +1,20 @@
+int main() {
+    char c;
+    scanf("%c", &c);
+    if(c == 'A'){
+        printf("Excellent");
+    }
+    else if(c == 'B'){
+        printf("Good");
+    }
+    else if(c == 'C'){
+        printf("Average");
+    }
+    else if(c == 'D'){
+        printf("Below Average");
+    }
+    else if(c == 'F'){
+        printf("Fail");
+    }
+    return =0;
+}
