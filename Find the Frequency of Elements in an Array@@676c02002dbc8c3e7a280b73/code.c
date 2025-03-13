c3d073c0af06d319e1913a65837ebf421194303a
UJJWAL <ujjwal.1614@gmail.com>
2025-03-13 21:18:46 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -34,46 +34,43 @@ int main() {
             }
         }
     }
-    // int num = 1;
-    // for(int i = 0; i<n-1; i++)
-    // {
-    //     if(arr[i] != arr[i+1]){
-    //         num += 1;
-    //     }
-    // }
-    // int arr2[num];
-    // int arr3[num];
-    // arr2[0] = 1;
-    // int maa = 1;
-    // int vanshu = 0;
-    // arr3[0] = arr[0];
-    // for(int i =0; i<n; i++){
-    //         if(arr[i] != arr[i+1]){
-    //             arr3[maa] = arr[i+1];
-    //             maa += 1;
-    //         }
-    //     }
-    // for(int i = 0; i<n-1; i++){
-    //     for(int j = i+1; j<n; j++){
-    //         if(arr[0] == arr[n-1])
-    //         {
-    //             arr2[0] = n-1;
-    //         }
-    //         if(arr[i] == arr[j]){
-    //             arr2[vanshu] += 1;
-    //         }
-    //         else{
-    //             vanshu += 1;
-    //             arr2[vanshu] = 1;
-    //             i = j;
-    //         }
-    //     }
-    // }
-    // for(int i = 0; i<num; i++){
-    //     printf("%d %d\n", arr3[i], arr2[i]);
-    // }
-    for(int i = 0; i<n; i++){
-        printf("%d ", scam[i]);
+    int num = 1;
+    for(int i = 0; i<n-1; i++)
+    {
+        if(arr[i] != arr[i+1]){
+            num += 1;
+        }
+    }
+    int arr2[num];
+    int arr3[num];
+    arr2[0] = 1;
+    int maa = 1;
+    int vanshu = 0;
+    arr3[0] = arr[0];
+    for(int i =0; i<n; i++){
+            if(arr[i] != arr[i+1]){
+                arr3[maa] = arr[i+1];
+                maa += 1;
+            }
+        }
+    for(int i = 0; i<n-1; i++){
+        for(int j = i+1; j<n; j++){
+            if(arr[0] == arr[n-1])
+            {
+                arr2[0] = n-1;
+            }
+            if(arr[i] == arr[j]){
+                arr2[vanshu] += 1;
+            }
+            else{
+                vanshu += 1;
+                arr2[vanshu] = 1;
+                i = j;
+            }
+        }
+    }
+    for(int i = 0; i<num; i++){
+        printf("%d %d\n", scam[i], arr2[i]);
     }
     return 0;
 }
