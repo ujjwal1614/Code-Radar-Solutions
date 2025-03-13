f18797e3e4fc357d3c4e6b0b1435fc73454be5d2
UJJWAL <ujjwal.1614@gmail.com>
2025-03-13 21:13:58 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -7,39 +7,71 @@ int main() {
     for(int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
-
-    // Frequency array to store the count of each element
     int freq[n];
+    int uniqueCount = 0;
     for(int i = 0; i < n; i++) {
-        freq[i] = 0;  // Initialize frequency array
+        freq[i] = 0;
     }
-
-    // Count the frequency of each element in arr[]
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < i; j++) {
-            if(arr[i] == arr[j]) {
-                freq[i] = -1;  // Mark this element as already processed
-                break;
-            }
-        }
-        if(freq[i] != -1) {
-            for(int j = 0; j < n; j++) {
+        if(freq[i] == 0) {
+            int count = 1;
+            for(int j = i + 1; j < n; j++) {
                 if(arr[i] == arr[j]) {
-                    freq[i]++;
+                    count++;
+                    freq[j] = -1;
                 }
             }
+            freq[i] = count;
         }
     }
-
-    // Print the grouped numbers based on their frequency
+    int scam[n];
     for(int i = 0; i < n; i++) {
-        if(freq[i] != -1) {
-            // Print the number 'freq[i]' times based on its frequency
+        if(freq[i] > 0) {
             for(int j = 0; j < freq[i]; j++) {
-                printf("%d ", arr[i]);
+                scam[i] = arr[i];
             }
         }
     }
-
+    // int num = 1;
+    // for(int i = 0; i<n-1; i++)
+    // {
+    //     if(arr[i] != arr[i+1]){
+    //         num += 1;
+    //     }
+    // }
+    // int arr2[num];
+    // int arr3[num];
+    // arr2[0] = 1;
+    // int maa = 1;
+    // int vanshu = 0;
+    // arr3[0] = arr[0];
+    // for(int i =0; i<n; i++){
+    //         if(arr[i] != arr[i+1]){
+    //             arr3[maa] = arr[i+1];
+    //             maa += 1;
+    //         }
+    //     }
+    // for(int i = 0; i<n-1; i++){
+    //     for(int j = i+1; j<n; j++){
+    //         if(arr[0] == arr[n-1])
+    //         {
+    //             arr2[0] = n-1;
+    //         }
+    //         if(arr[i] == arr[j]){
+    //             arr2[vanshu] += 1;
+    //         }
+    //         else{
+    //             vanshu += 1;
+    //             arr2[vanshu] = 1;
+    //             i = j;
+    //         }
+    //     }
+    // }
+    // for(int i = 0; i<num; i++){
+    //     printf("%d %d\n", arr3[i], arr2[i]);
+    // }
+    for(int i = 0; i<num; i++){
+        printf("%d ", scam[i]);
+    }
     return 0;
 }
