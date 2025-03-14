d90b24b15331dc65893f625c14131b73d71e03d8
UJJWAL <ujjwal.1614@gmail.com>
2025-03-14 13:57:45 +0530

    Solution to Find the Second Largest Element in an Array

--- Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
+++ Find the Second Largest Element in an Array@@676c02002dbc8c3e7a280b72/code.c
@@ -1,44 +1,3 @@
-// #include<stdio.h>
-// int main(){
-//     int n;
-//     scanf("%d", &n);
-//     int arr[n];
-//     for(int i = 0; i<n; i++){
-//         scanf("%d", &arr[i]);
-//     }
-//     int ujjwal;
-//     for(int i = 0; i<n-1; i++){
-//         for(int j = i+1; j<n; j++){
-//         if(arr[i] > arr[j]){
-//             arr[i] = ujjwal;
-//             arr[i] = arr[j];
-//             arr[j] = ujjwal;
-//         }
-//     }
-//     }
-//     int count = 1;
-//     for(int i = 0; i<n; i++){
-//         for(int j = 1; j<n; j++){
-//             if(arr[0] == arr[j]){
-//                 count += 1;
-//                 return 0;
-//             }
-//         }
-//     }
-//     if(count == n){
-//         printf("-1");
-//         return 0;
-//     }
-//     else if(arr[0]>0){
-//         printf("%d ", arr[n-2]);
-//     return 0;
-//     }
-//     else{
-//         printf("%d ", arr[1]);
-//     return 0;
-//     } 
-//     return 0;
-// }
 #include<stdio.h>
 int main() {
     int n;
@@ -72,8 +31,16 @@ int main() {
             scamcount += 1;
         }
     }
-    for(int i = 0; i<scamcount; i++){
-        printf("%d ", scam[i]);
+    if(scamcount = 1){
+        printf("-1");
     }
+    else if(arr[0]>0){
+        printf("%d ", scam[scamcount-2]);
+    return 0;
+    }
+    else{
+        printf("%d ", scam[1]);
+    return 0;
+    } 
     return 0;
 }
