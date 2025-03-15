5809514320c96182060061a99b68c98835f1feb3
UJJWAL <ujjwal.1614@gmail.com>
2025-03-15 20:13:29 +0530

    Solution to Replace Each Element with the Greatest Element on its Right

--- Replace Each Element with the Greatest Element on its Right@@676c02002dbc8c3e7a280b84/code.c
+++ Replace Each Element with the Greatest Element on its Right@@676c02002dbc8c3e7a280b84/code.c
@@ -10,9 +10,7 @@ int main() {
     for(int i = 0; i<n-1; i++){
         for(int j = i+1; j<n; j++){
             if(arr[i] < arr[j]){
-                ujjwal = arr[i];
                 arr[i] = arr[j];
-                arr[j] = ujjwal;
                 break;
             }
         }
